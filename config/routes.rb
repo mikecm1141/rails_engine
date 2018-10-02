1161f0fa8eea41a5e8d8a273b2190fd0ae045b82
Mike McKee <mikecm@gmail.com>
2018-10-02 08:38:41 -0600

    Adds record endpoint for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 		namespace :v1 do
 			resources :merchants, only: %i[index show]
       resources :items, only: %i[index show]
+      resources :invoices, only: %i[index show]
 		end
 	end
 end
