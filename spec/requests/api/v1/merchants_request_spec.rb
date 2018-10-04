9cd2c350992a8aee7cae2eada94928ecf88834e8
Mike McKee <mikecm@gmail.com>
2018-10-03 22:32:38 -0600

    add context to merchants request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  it 'sends a list of all merchants' do
-    create_list(:merchant, 3)
+  context 'get /api/v1/merchants/' do
+    it 'sends a list of all merchants' do
+      create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
-    merchants = JSON.parse(response.body)
+      get '/api/v1/merchants'
+      merchants = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(merchants.count).to eq(3)
+      expect(response).to be_successful
+      expect(merchants.count).to eq(3)
+    end
   end
 
-  it 'can show one merchant by id' do
-    id = create(:merchant).id
+  context 'get /api/v1/merchants/:id' do
+    it 'can show one merchant by id' do
+      id = create(:merchant).id
 
-    get "/api/v1/merchants/#{id}"
+      get "/api/v1/merchants/#{id}"
+      merchant = JSON.parse(response.body)
 
-    merchant = JSON.parse(response.body)
-
-    expect(response).to be_successful
-    expect(merchant['id']).to eq(id)
+      expect(response).to be_successful
+      expect(merchant['id']).to eq(id)
+    end
   end
 end
