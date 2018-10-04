3b9266a866374f8464319686d4ef4370eb2f7571
Mike McKee <mikecm@gmail.com>
2018-10-03 22:29:06 -0600

    add context to customer request spec

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 describe 'Customer API' do
-  it 'sends a list of all customers' do
-    create_list(:customer, 3)
+  context 'get /api/v1/customers' do
+    it 'sends a list of all customers' do
+      create_list(:customer, 3)
 
-    get '/api/v1/customers'
-    customers = JSON.parse(response.body)
+      get '/api/v1/customers'
+      customers = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(customers.count).to eq(3)
+      expect(response).to be_successful
+      expect(customers.count).to eq(3)
+    end
   end
 
-  it 'can show one customer by id' do
-    id = create(:customer).id
+  context 'get /api/v1/customers/:d' do
+    it 'can show one customer by id' do
+      id = create(:customer).id
 
-    get "/api/v1/customers/#{id}"
-    customer = JSON.parse(response.body)
+      get "/api/v1/customers/#{id}"
+      customer = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(customer['id']).to eq(id)
+      expect(response).to be_successful
+      expect(customer['id']).to eq(id)
+    end
   end
 end
