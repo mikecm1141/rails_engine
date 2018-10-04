65510d0b82e290a6a0dcd78fc0d8afde3d32dc3f
Mike McKee <mikecm@gmail.com>
2018-10-03 22:33:49 -0600

    adds context to transactions request spec

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 describe 'Transactions API' do
-  it 'sends a list of all transactions' do
-    create_list(:transaction, 3)
+  context 'get /api/v1/transactions' do
+    it 'sends a list of all transactions' do
+      create_list(:transaction, 3)
 
-    get '/api/v1/transactions'
-    transactions = JSON.parse(response.body)
+      get '/api/v1/transactions'
+      transactions = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(transactions.count).to eq(3)
+      expect(response).to be_successful
+      expect(transactions.count).to eq(3)
+    end
   end
 
-  it 'can show one transaction by id' do
-    id = create(:transaction).id
+  context 'get /api/v1/transactions/:id' do
+    it 'can show one transaction by id' do
+      id = create(:transaction).id
 
-    get "/api/v1/transactions/#{id}"
+      get "/api/v1/transactions/#{id}"
+      transaction = JSON.parse(response.body)
 
-    transaction = JSON.parse(response.body)
-
-    expect(response).to be_successful
-    expect(transaction['id']).to eq(id)
+      expect(response).to be_successful
+      expect(transaction['id']).to eq(id)
+    end
   end
 end
