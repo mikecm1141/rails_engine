4294eb131987b628616024321dde3d5b3f2a663b
Mike McKee <mikecm@gmail.com>
2018-10-03 22:30:03 -0600

    add context to invoice item request spec

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 describe 'Invoice Items API' do
-  it 'sends a list of all invoice items' do
-    create_list(:invoice_item, 3)
+  context 'get /api/v1/invoice_items' do
+    it 'sends a list of all invoice items' do
+      create_list(:invoice_item, 3)
 
-    get '/api/v1/invoice_items'
-    invoice_items = JSON.parse(response.body)
+      get '/api/v1/invoice_items'
+      invoice_items = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(invoice_items.count).to eq(3)
+      expect(response).to be_successful
+      expect(invoice_items.count).to eq(3)
+    end
   end
 
-  it 'can show one invoice item by id' do
-    id = create(:invoice_item).id
+  context 'get /api/v1/invoice_items/:id' do
+    it 'can show one invoice item by id' do
+      id = create(:invoice_item).id
 
-    get "/api/v1/invoice_items/#{id}"
-    invoice_item = JSON.parse(response.body)
+      get "/api/v1/invoice_items/#{id}"
+      invoice_item = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    expect(invoice_item['id']).to eq(id)
+      expect(response).to be_successful
+      expect(invoice_item['id']).to eq(id)
+    end
   end
 end
