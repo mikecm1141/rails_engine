c5f802e8362d55ed3dd6ebb960e8b67cbf76b5aa
Mike McKee <mikecm@gmail.com>
2018-10-06 11:40:01 -0600

    removing comments

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    # best_day = Item.best_day(params[:item_id])
-    # render json: { "best_day": best_day }
     render json: Item.best_day(params[:item_id]),
                  serializer: ItemsBestDaySerializer
   end
